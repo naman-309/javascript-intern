4a60b681be067686f0feb0b80719cc91a348de6a
Naman Patidar <namanpatidar309@gmail.com>
2026-04-07 16:11:08 +0530

    hello commit

---
+++ loop/introloop.js
@@ -0,0 +1,34 @@
+
+
+// // 1  to 70 odd ever
+// // for (i = 100; i >= 70; i--) {
+// //     if (i % 2 === 0) {
+// //         console.log("even number ", i)
+// //     }
+// //     else {
+// //         console.log("odd  number", i)
+
+// //     }
+// // }
+
+
+// /// sum  of   1  to 10
+// //ptint  the  total muntiple of  1 to 10
+
+// let sum = 0
+// for (let a = 1; i <= 10) {
+
+//     //
+
+
+//     /// add  numbr  divisible  with 1 to 30 by  3
+//     let add = 0;
+//     for (i = 1; i <= 30; i++) {
+
+//         if (i % 3 == 0) {
+//             add += i;
+//         }
+//         console.log(add)
+//     }
+// }
+
