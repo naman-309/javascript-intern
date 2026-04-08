eecca1280c6d57d60c657df31a4e58270cd3e180
Naman Patidar <namanpatidar309@gmail.com>
2026-04-08 16:05:03 +0530

    hello nested app

--- loop/introloop.js
+++ loop/introloop.js
@@ -1,34 +1,90 @@
+// // print the  table
+// let num = 2;
+// for (i = 1; i <= 10; i++) {
 
+//     console.log(`${num} x ${i} = ${i * num}`)
 
-// // 1  to 70 odd ever
-// // for (i = 100; i >= 70; i--) {
-// //     if (i % 2 === 0) {
-// //         console.log("even number ", i)
-// //     }
-// //     else {
-// //         console.log("odd  number", i)
+// }
 
-// //     }
-// // }
+// // print even and  odd  number 
 
+// for (i = 1; i <= 20; i++) {
 
-// /// sum  of   1  to 10
-// //ptint  the  total muntiple of  1 to 10
-
-// let sum = 0
-// for (let a = 1; i <= 10) {
-
-//     //
-
-
-//     /// add  numbr  divisible  with 1 to 30 by  3
-//     let add = 0;
-//     for (i = 1; i <= 30; i++) {
-
-//         if (i % 3 == 0) {
-//             add += i;
-//         }
-//         console.log(add)
+//     if (i % 2 == 0) {
+//         console.log('even number', i)
+//     }
+//     else {
+//         console.log("odd numbner", i)s
 //     }
 // }
 
+
+// print totl -  add the number divisble  by 3 
+
+let sum = 0;
+for (i = 1; i <= 50; i++) {
+    if (i % 3 === 0) {
+
+        sum += i
+    }
+}
+console.log("total number  for  3:", sum)
+
+// 1–100 me se 5 se divisible numbers ka sum nikalo
+let a = 0;
+for (i = 1; i <= 100; i++) {
+    if (i % 5 === 0) {
+        a += i;
+    }
+}
+console.log("total number for  5 :", a)
+
+//1–50 me se even numbers ka sum nikalo
+
+let sumeven = 0;
+for (i = 1; i <= 50; i++) {
+    if (i % 2 === 0) {
+        sumeven += i;
+    }
+}
+console.log("total sum for  even :", sumeven)
+
+//1–50 me se odd numbers count karo
+let countodd = 0;
+for (i = 1; i <= 50; i++) {
+    if (i % 2 === 1) {
+        countodd++;
+    }
+}
+console.log("count  number  of odd :", countodd)
+
+//  1–20 me se numbers ka square ka sum nikalo
+
+let summofsq = 0;
+for (i = 1; i <= 20; i++) {
+    let square = i * i
+    summofsq = summofsq + square
+
+}
+console.log("summ of  square :", summofsq)
+
+
+
+// 1-3 ke cube  roots  ka  sum 
+
+let cubestart = 0;
+for (i = 1; i <= 3; i++) {
+    let cube = i * i * i
+    cubestart = cubestart + cube
+
+}
+
+//Ek ecommerce site par 1–50 order IDs hain.
+//Agar order ID 5 se divisible hai to us order par
+//₹100 discount apply hota hai.
+
+
+totaldiscount = 0;
+for (i = 1; i <= 50; i++) {
+
+}
---
+++ loop/nesedloop.js
@@ -0,0 +1,30 @@
+//  basic  question 
+/// outr lopp 2 time and ineer  loop 3  times  
+// also counting the  run time  of  ineer  
+let count = 0
+for (i = 1; i <= 2; i++) {
+    console.log(" outer  loop", i)
+    for (j = 1; j <= 3; j++) {
+        console.log('ineerloop ', j)
+        count++
+    }
+
+}
+console.log('count inner  loop', count)
+console.log('end')
+
+//  print  numner  wen i less thqn j "
+// :is  smaller  then j - run 10 to  10  
+for (i = 10; i <= 10; i++) {
+
+    for (j = 1; j <= 10; j++) {
+        if (i, j) {
+
+            console.log(`i:${i} and j:${j}`)
+        }
+    }
+}
+
+
+
+
