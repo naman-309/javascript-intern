b6cdf8bd71c2ca74202296f56c6839c95b5abb0a
Naman Patidar <namanpatidar309@gmail.com>
2026-05-01 16:03:40 +0530

    hello

--- array/introarray.js
+++ array/introarray.js
@@ -1,6 +1,76 @@
-
+// 
 
 let arry = [1, 2, 3, 4, "hello"]
 console.log(arry.length)
 console.log(arry.indexOf("hello"))
 console.log(arry)
+
+// methods
+//push -to add foprm last
+arry.push(23, 40)
+console.log(arry)
+
+//pop - remove  - form last 
+
+arry.pop()
+console.log(arry)
+
+//unshift - to add form starting 
+
+arry.unshift(1, 3)
+console.log(arry)
+
+// shift  - to remvoe form starting
+arry.shift()
+console.log(arry)
+
+// slice
+// inclue  element  with starting  index  
+// exclude  the  last index  which me  metion
+// original array  dose not affect 
+
+let app = [1, 2, 3, 4, 5, 6, 7]
+console.log(app.slice(1, 5))
+// lets  make  new  sub arry form slice 
+
+let appdemo = app.slice(1, 4)
+console.log(appdemo)
+
+
+//splice  
+// aftect the 0riginal arry 
+// can add  new element  by  using  3rd  paramaeter in method  
+// can make new  sub arry 
+console.log("splice")
+let ren = [1, 2, 3, 4, 5, 6, 7]
+ren.splice(1, 2)
+console.log(ren)
+
+let rendemo = ren.splice(1, 2, 777, 888, 999)  // to make  new  arry and  add  more  element
+console.log(rendemo)
+console.log(ren)  // added  new  elements  
+
+
+
+// sum of array 
+let ren1 = ["1", "2", "3", "4"]
+let print = ""
+for (i = 0; i <= ren1.length - 1; i++) {
+
+    print += ren1[i]
+}
+console.log("element  print:", print)
+
+
+let ren11 = ["1", "2", "3", "4"]
+let rev = ""
+for (i = ren11.length - 1; i >= 0; i--) {
+
+    rev += ren11[i]
+}
+console.log("reverse  :", rev)
+
+
+
+
+
